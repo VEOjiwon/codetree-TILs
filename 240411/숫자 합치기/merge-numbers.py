86fd9dbb03a99ac26294d7088c72f10abf950dcc
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-11 01:08:28 +0900

    Title: 숫자 합치기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240411/숫자 합치기/merge-numbers.py
+++ 240411/숫자 합치기/merge-numbers.py
@@ -1,25 +1,20 @@
 import sys
+import heapq
+
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
 
-nums.sort()
-cost = 0
-tmp = nums[0] + nums[1]
-cost += tmp
-nums = nums[2:]
-for i in range(len(nums)//2):
-    if tmp < nums[1]:
-        tmp += nums[0]
-        cost = tmp + nums[0]
-        nums = nums[1:]
-    else:
-        val = nums[0] + nums[1]
-        tmp = val
-        cost += val
-        nums = nums[2:]
-    
-    
-    
+q = []
+ans = 0
 
-cost += sum(nums)
-print(cost)
+for num in nums:
+    heapq.heappush(q, num)
+
+while len(pq) > 1:
+    x1 = heapq.heappop(pq)
+    x2 = heapq.heappop(pq)
+
+    ans += (x1+x2)
+    heapq.hezppus(pq,x1+x2)
+
+print(ans)
