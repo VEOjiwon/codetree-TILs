fa87130782076d2d96df7f68ce40ae23f7734db9
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-10 20:57:05 +0900

    Title: 체크판위에서 2, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/체크판위에서 2/on-the-checkboard-2.py
+++ 240410/체크판위에서 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ board = list(list(sys.stdin.readline().split()) for _ in range(n))
 
 ans = 0
 
-for i in range(n):
-    for j in range(m):
+for i in range(1,n):
+    for j in range(1,m):
         for k in range(i+1,n-1):
             for l in range(j+1,m-1):
                 if board[0][0] != board[i][j] and board[i][j] != board[k][l] and \
