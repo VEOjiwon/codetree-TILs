942d802eb31ce82c2f72a492d21a6c5c21343922
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-10 20:56:33 +0900

    Title: 체크판위에서 2, Time: 106ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240410/체크판위에서 2/on-the-checkboard-2.py
+++ 240410/체크판위에서 2/on-the-checkboard-2.py
@@ -2,15 +2,13 @@ import sys
 n,m = map(int, sys.stdin.readline().split())
 board = list(list(sys.stdin.readline().split()) for _ in range(n))
 
-ws = []
-bs = []
-cur_c = board[0][0]
 ans = 0
-for i in range(n-1):
-    for j in range(m-1):
-        for k in range(i+1,n-2):
-            for l in range(j+1,m-2):
-                if board[k][l] != board[i][j]:
+
+for i in range(n):
+    for j in range(m):
+        for k in range(i+1,n-1):
+            for l in range(j+1,m-1):
+                if board[0][0] != board[i][j] and board[i][j] != board[k][l] and \
+                    board[k][l] != board[n-1][m-1]:
                     ans+=1
-    
 print(ans)
