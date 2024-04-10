f6e70ca2e8794764fed16f5078d4c99cd4a0e4de
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-10 22:04:43 +0900

    Title: 동전 더하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/동전 더하기/add-coins.py
@@ -0,0 +1,15 @@
+import sys
+n, k = map(int,sys.stdin.readline().split())
+money = list(int(sys.stdin.readline()) for _ in range(n))
+
+money.sort(reverse=True)
+
+tmp = k
+ans = 0
+for val in money:
+    if tmp == 0:
+        print(ans)
+        break
+    if tmp // val > 0:
+        ans = ans + (tmp//val)
+        tmp = tmp % val
