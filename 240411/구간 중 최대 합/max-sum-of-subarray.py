8c9ea090713ed39ef479784f517ab3bcda7ab604
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-11 01:30:49 +0900

    Title: 구간 중 최대 합, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/구간 중 최대 합/max-sum-of-subarray.py
+++ 240411/구간 중 최대 합/max-sum-of-subarray.py
@@ -3,6 +3,6 @@ import sys
 n,k = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 res = 0
-for i in range(n-k):
-    res = max(res, sum(arr[i:i+k+1]))
+for i in range(n-k+1):
+    res = max(res, sum(arr[i:i+k]))
 print(res)
