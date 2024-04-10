ccfddcc7572a3b57833e743ea0c4e59c4d68014d
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-10 22:24:52 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline())
+vec = list(map(int, sys.stdin.readline().split()))
+ans = -1248000
+i = 0
+while i <n:
+    
+    range_sum = vec[i]
+    local_max = -127000
+    
+    for k in range(i+1,n):
+        range_sum += vec[k]
+        if range_sum < 0:
+            i+=k
+            break
+        local_max = max(local_max,range_sum)
+        
+
+    ans = max(ans, local_max)
+    i+=1
+
+print(ans)
