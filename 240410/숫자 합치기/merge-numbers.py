4c2a8ba3b82623d8226b55b5c281b69024633aad
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-10 23:32:36 +0900

    Title: 숫자 합치기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/숫자 합치기/merge-numbers.py
+++ 240410/숫자 합치기/merge-numbers.py
@@ -1,13 +1,15 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
+nums.sort()
 cost = 0
-for _ in range(n//2):
-    nums.sort()
-    cost += nums[0] + nums[1]
-    nums.append(nums[0]+nums[1])
+for i in range(n//2):
+    val = nums[0] + nums[1]
+    cost += val
     nums = nums[2:]
-    
+    for j in range(len(nums)):
+        if nums[j] < val:
+            nums.insert(j-1,val)
 
 cost += sum(nums)
 print(cost)
