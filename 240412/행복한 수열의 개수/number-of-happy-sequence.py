e08d42fd8edbb3ce203cf666ce64dd3f9d5dca1f
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-12 21:05:53 +0900

    Title: 행복한 수열의 개수, Time: 165ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240412/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240412/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,32 +3,34 @@ n, m = map(int,sys.stdin.readline().split())
 board = list(list(map(int,sys.stdin.readline().split())) for _ in range(n))
 
 ans = 0
-
-for i in range(n):
-    tmp=1
-    tmax = 0
-    cmax = 0
-    ctmp = 1
-    for j in range(n):
-        if i ==0:
+if m == 1:
+    print(2*n)
+else:
+    for i in range(n):
+        tmp=1
+        tmax = 0
+        cmax = 0
+        ctmp = 1
+        for j in range(n):
+            if i ==0:
+                prev2 = board[j][i]
+            elif prev2 == board[j][i]:
+                ctmp +=1
+                cmax = max(cmax,ctmp)
+            else:
+                ctmp = 1
             prev2 = board[j][i]
-        elif prev2 == board[j][i]:
-            ctmp +=1
-            cmax = max(cmax,ctmp)
-        else:
-            ctmp = 1
-        prev2 = board[j][i]
-        if j==0:
+            if j==0:
+                prev = board[i][j]
+            elif prev == board[i][j]:
+                tmp+=1
+                tmax = max(tmax,tmp)
+            else:
+                tmp = 1
             prev = board[i][j]
-        elif prev == board[i][j]:
-            tmp+=1
-            tmax = max(tmax,tmp)
-        else:
-            tmp = 1
-        prev = board[i][j]
 
-    if tmax>=m:
-        ans+=1
-    if cmax>=m:
-        ans+=1
-print(ans)
+        if tmax>=m:
+            ans+=1
+        if cmax>=m:
+            ans+=1
+    print(ans)
