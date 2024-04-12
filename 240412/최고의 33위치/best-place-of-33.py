4be88d2fe2f14fb9503e61f0e4b516c58c149fb4
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-12 20:36:46 +0900

    Title: 최고의 33위치, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240412/최고의 33위치/best-place-of-33.py
+++ 240412/최고의 33위치/best-place-of-33.py
@@ -3,5 +3,10 @@ board=list(list(map(int, input().split()))for _ in range(n))
 res = 0
 for i in range(n-2):
     for j in range(n-2):
-        res = max(res,sum(map(sum,board[i:i+3][j:j+3])))
+        tmp = 0
+        for k in range(i,i+3):
+            for l in range(j,j+3):
+                tmp+= board[k][l]
+        res = max(res,tmp)
+
 print(res)
