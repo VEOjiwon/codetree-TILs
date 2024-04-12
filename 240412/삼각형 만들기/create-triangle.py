cb7c708a2b1f400a79f0e011e21792434787b09d
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-12 20:24:39 +0900

    Title: 삼각형 만들기, Time: 178ms, Memory: 26MB, Status: Passed - CodeTree

--- 240412/삼각형 만들기/create-triangle.py
+++ 240412/삼각형 만들기/create-triangle.py
@@ -12,6 +12,6 @@ for i in range(n):
             x1, y1 = cords[i]
             x2, y2 = cords[j]
             x3, y3 = cords[k]
-            if (y1 == y2 and x2 == x3) or (y1 == y2 and x1 == x3) or (y2 == y3 and x2 == x1) or (y2 == y3 and x3 == x1) or (y1 == y3 and x1 == x2) or (y1 == y3 and x1 == x3):
+            if (x1==x2 or x1==x3 or x2==x3) and (y1==y2 or y1==y3 or y2==y3):
                 res = max(res,area(x1,x2,x3,y1,y2,y3))
 print(res)
