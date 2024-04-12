2a2f0abffa08da768fafd9236bed249eee80a6bc
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-12 20:21:58 +0900

    Title: 삼각형 만들기, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240412/삼각형 만들기/create-triangle.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+cords = list(list(map(int,sys.stdin.readline().split())) for _ in range(n))
+
+def area(x1,x2,x3,y1,y2,y3):
+    return abs((x1*y2+x2*y3+x3*y1) - (x2*y1+x3*y2+x1*y3))
+res = 0
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            x1, y1 = cords[i]
+            x2, y2 = cords[j]
+            x3, y3 = cords[k]
+            if y1 == y2 and x2 == x3 or (y1 == y2 and x1 == x3) or (y2 == y3 and x2 == x1) or (y2 == y3 and x3 == x1) or (y1 == y3 and x1 == x2) or (y1 == y3 and x1 == x3):
+            
+
+                res = max(res,area(x1,x2,x3,y1,y2,y3))
+print(res)
