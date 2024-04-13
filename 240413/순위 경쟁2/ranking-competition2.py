9efa01851a337aea8fc04231baa38a500656d98e
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-13 21:34:01 +0900

    Title: 순위 경쟁2, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240413/순위 경쟁2/ranking-competition2.py
+++ 240413/순위 경쟁2/ranking-competition2.py
@@ -3,22 +3,14 @@ n = int(input())
 cmd = list(list(input().split()) for _ in range(n))
 
 ranks = {'A':0, 'B':0}
-cur = 0
+cur = 'AB'
 cnt = 0
 for i in range(n):
     p, num = cmd[i]
     num = float(num)
     
     ranks[p] += num
-    if cur == 0:
-        cnt+=1
-        if ranks['A'] > ranks['B']:
-            cur = 'A'
-        elif ranks['A'] < ranks['B']:
-            cur = 'B'
-        else:
-            cur ='AB'
-    elif ranks['A'] > ranks['B'] and (cur=='B' or cur =='AB'):
+    if ranks['A'] > ranks['B'] and (cur=='B' or cur =='AB'):
         cur = 'A'
         cnt+=1
     elif ranks['A'] < ranks['B'] and (cur=='A'or cur =='AB'):
