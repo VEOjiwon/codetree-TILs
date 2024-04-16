dd3a38505a9e9ae5d42f2ea8307a00b208f28363
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-16 21:11:07 +0900

    Title: 별표 출력하기 13, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/별표 출력하기 13/print-start-13.py
+++ 240416/별표 출력하기 13/print-start-13.py
@@ -1,13 +1,17 @@
 n = int(input())
 
-for i in range(n,1,-1):
-    print('* '*i)
-    if i ==2:
-        continue
+if n == 1:
     print('*')
+    print('*')
+else:
+    for i in range(n,1,-1):
+        print('* '*i)
+        if i ==2:
+            continue
+        print('*')
 
-for i in range(2,n+1):
-    print('* '*i)
-    if i ==n:
-        continue
-    print('*')
+    for i in range(2,n+1):
+        print('* '*i)
+        if i ==n:
+            continue
+        print('*')
