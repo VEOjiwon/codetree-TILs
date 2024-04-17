bdab31594cfecee8fa7fca510b696b4e13300f89
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-17 22:07:10 +0900

    Title: 미는 횟수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240417/미는 횟수/number-of-pushes.py
@@ -0,0 +1,11 @@
+a = input().rstrip()
+b = input().rstrip()
+
+tmp = a
+for i in range(1,len(a)+1):
+    tmp=tmp[1:] + tmp[0]
+    if tmp == b:
+        print(i)
+        break
+else:
+    print(-1)
