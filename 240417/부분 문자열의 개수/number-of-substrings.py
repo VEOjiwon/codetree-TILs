59d46a60182ef657ab7214cbcda5f89d18991375
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-17 21:30:38 +0900

    Title: 부분 문자열의 개수, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240417/부분 문자열의 개수/number-of-substrings.py
+++ 240417/부분 문자열의 개수/number-of-substrings.py
@@ -1,4 +1,8 @@
 str1 = input().rstrip()
 target = input().rstrip()
 
-print(str1.count(target))
+cnt = 0
+for i in range(len(str1)-1):
+    if str1[i] + str1[i+1] == target:
+        cnt+=1
+print(cnt)
