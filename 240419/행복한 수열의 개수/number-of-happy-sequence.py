9d7eccd23cd406732162e37362b6df301fa2c3fb
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-19 00:50:42 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240419/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240419/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,11 +1,12 @@
 n,k = map(int, input().split())
 board = list(list(map(int, input().split())) for _ in range(n))
 ans = 0
+
 for i in range(n):
     max_conseq = 1
     local_max = 0
-    for j in range(n-1):
-        if board[i][j] == board[i][j+1]:
+    for j in range(1, n+1):
+        if board[i][j-1] == board[i][j]:
             max_conseq +=1
             
         else:
@@ -17,8 +18,8 @@ for i in range(n):
 for i in range(n):
     max_conseq = 1
     local_max = 0
-    for j in range(n-1):
-        if board[j][i] == board[j+1][i]:
+    for j in range(1, n+1):
+        if board[j][i] == board[j-1][i]:
             max_conseq +=1
         else:
             max_conseq=1
