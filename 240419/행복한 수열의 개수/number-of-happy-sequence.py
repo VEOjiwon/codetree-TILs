4efd0eb7736344748ec06a626f23511a5be4ea53
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-19 00:51:39 +0900

    Title: 행복한 수열의 개수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240419/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,28 +2,32 @@ n,k = map(int, input().split())
 board = list(list(map(int, input().split())) for _ in range(n))
 ans = 0
 
-for i in range(n):
-    max_conseq = 1
-    local_max = 0
-    for j in range(1, n):
-        if board[i][j-1] == board[i][j]:
-            max_conseq +=1
-            
-        else:
-            max_conseq=1
-        local_max = max(local_max, max_conseq)
-    if local_max >=k:
-        ans+=1
+if k == 1:
+    print(n*n)
+else:
 
-for i in range(n):
-    max_conseq = 1
-    local_max = 0
-    for j in range(1, n):
-        if board[j][i] == board[j-1][i]:
-            max_conseq +=1
-        else:
-            max_conseq=1
-        local_max = max(local_max, max_conseq)
-    if local_max >=k:
-        ans+=1
-print(ans)
+    for i in range(n):
+        max_conseq = 1
+        local_max = 0
+        for j in range(1, n):
+            if board[i][j-1] == board[i][j]:
+                max_conseq +=1
+                
+            else:
+                max_conseq=1
+            local_max = max(local_max, max_conseq)
+        if local_max >=k:
+            ans+=1
+
+    for i in range(n):
+        max_conseq = 1
+        local_max = 0
+        for j in range(1, n):
+            if board[j][i] == board[j-1][i]:
+                max_conseq +=1
+            else:
+                max_conseq=1
+            local_max = max(local_max, max_conseq)
+        if local_max >=k:
+            ans+=1
+    print(ans)
