9d82b2982e8e7e55dca456e540ffc9417fe2f2d8
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-19 00:47:58 +0900

    Title: 행복한 수열의 개수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240419/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,21 +3,25 @@ board = list(list(map(int, input().split())) for _ in range(n))
 ans = 0
 for i in range(n):
     max_conseq = 1
+    local_max = 0
     for j in range(n-1):
         if board[i][j] == board[i][j+1]:
             max_conseq +=1
+            local_max = max(local_max, max_conseq)
         else:
             max_conseq=1
-    if max_conseq >=k:
+    if local_max >=k:
         ans+=1
 
 for i in range(n):
     max_conseq = 1
+    local_max = 0
     for j in range(n-1):
         if board[j][i] == board[j+1][i]:
             max_conseq +=1
+            local_max = max(local_max, max_conseq)
         else:
             max_conseq=1
-    if max_conseq >=k:
+    if local_max >=k:
         ans+=1
 print(ans)
