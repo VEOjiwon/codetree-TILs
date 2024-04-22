84b29c020f983397a179bc0eb6018f4d6f4841c1
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-04-22 19:47:39 +0900

    Title: 트로미노, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240422/트로미노/tromino.py
@@ -0,0 +1,30 @@
+import sys
+n,m = map(int,input().split())
+board = list(list(map(int, input().split())) for _ in range(n))
+
+ans = -1
+
+for i in range(n):
+    for j in range(m):
+
+        if i < n-1 and j < m-1:
+            # 첫 번쨰
+            tmp = 0
+            tmp_min=sys.maxsize
+            for k in range(i,i+2):
+                for l in range(j,j+2):
+                    tmp+=board[k][l]
+                    tmp_min = min(tmp_min, board[k][l])
+            ans = max(ans, tmp - tmp_min)
+        # 두 번째
+        if j < n - 3:
+            tmp = 0
+            for k in range(j,j+3):
+                tmp+= board[i][k]
+            ans = max(ans, tmp - tmp_min)
+        if i < n - 3:
+            tmp = 0
+            for k in range(i,i+3):
+                tmp+= board[k][j]
+            ans = max(ans, tmp - tmp_min)
+print(ans)
