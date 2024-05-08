8de46521661c8fb41be631c9b9424ee032c7d933
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-05-08 17:10:37 +0900

    Title: 체크판위에서 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240508/체크판위에서 2/on-the-checkboard-2.py
+++ 240508/체크판위에서 2/on-the-checkboard-2.py
@@ -6,7 +6,7 @@ for i in range(1,n):
     for j in range(1, m):
         for k in range(i+1, n-1):
             for l in range(j+1,m-1):
-                if board[0][0] != board[i][j] and board[i][j] != board[k][j] and board[k][l] != board[n-1][m-1]:
+                if board[0][0] != board[i][j] and board[i][j] != board[k][l] and board[k][l] != board[n-1][m-1]:
                     cnt+=1
 
 print(cnt)
