c746509587e5fb32a7fad3abcc21f932d3941145
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-05-08 17:08:55 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240508/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,12 @@
+r,c = map(int, input().split())
+board = list(list(input().split()))
+
+cnt = 0
+for i in range(1,n):
+    for j in range(1, m):
+        for k in range(i+1, n-1):
+            for l in range(j+1,m-1):
+                if board[0][0] != board[i][j] and board[i][j] != board[k][j] and board[k][l] != board[n-1][m-1]:
+                    cnt+=1
+
+print(cnt)
