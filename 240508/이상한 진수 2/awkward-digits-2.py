d3d20bcfb874af02637c468341aa751e8b4f0461
Ji Won Kim <57858494+VEOjiwon@users.noreply.github.com>
2024-05-08 17:20:03 +0900

    Title: 이상한 진수 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,17 @@
+arr = list(input())
+
+def convert_decimal(arr):
+    arr.reverse()
+    num = 0
+    for i in range(len(arr)):
+        num += ((i)**2)*int(arr[i])
+    return num
+ans = 0
+for i in range(len(arr)):
+    
+    if arr[i] == '0':
+        arr[i] ='1'
+        num = convert_decimal(arr)
+        ans = max(ans, num)
+        arr[i] = '0'
+print(ans)
